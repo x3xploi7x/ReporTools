de302e75188eca8de39283a84c133060f92777e2
x3xploi7x <82796954+x3xploi7x@users.noreply.github.com>
2021-11-03 15:57:02 -0600

    Update ReporTools.js

--- src/dist/js/ReporTools.js
+++ src/dist/js/ReporTools.js
@@ -47,17 +47,18 @@
         }, false);
     }
 
-    loader(action) {
+    loader(action, color) {
         const parent = document.getElementById("container__modal");
         action = typeof(action) === "string" ? String(action) : Object(action);
 
         switch(action) {
             case "on":
                 parent.classList.add("display:block");
-                parent.innerHTML = `<div id="modal__loader" class="spinner:dashed"></div>`;
+                parent.innerHTML = `<div id="modal__loader" class="spinner:dotted spinner:red"></div>`;
                 break;
             
             case "off":
+                color = undefined;
                 parent.classList.remove("display:block");
                 parent.innerHTML = '';
                 break;
