c2456881d60663efe12d512b02a0cfd5ab2d478e
x3xploi7x <82796954+x3xploi7x@users.noreply.github.com>
2021-12-07 16:46:32 -0600

    Update spinner.js

--- src/util/assets/js/components/spinner.js
+++ src/util/assets/js/components/spinner.js
@@ -1,17 +1,29 @@
-class Spinner {
-    super();
-    
-    loader(action) {
+export default class Spinner {
+    loader(action, color) {
         const parent = document.getElementById("container__modal");
+        var child;
+
+        if(!document.getElementById(parent).length) {
+            const modal = document.createElement("div");
+                  modal.setAttribute("id", "container__modal");
+                  modal.setAttribute("class", "group:mirror");
+            child = modal;
+        } else {
+            modal = document.querySelector("group:mirror");
+            child = modal;
+        }
+    
+        document.getElementById(parent).appendChild(child);
         action = typeof(action) === "string" ? String(action) : Object(action);
 
         switch(action) {
             case "on":
                 parent.classList.add("display:block");
-                parent.innerHTML = `<div id="modal__loader" class="spinner:dashed"></div>`;
+                parent.innerHTML = `<div id="modal__loader" class="spinner:dotted spinner:red"></div>`;
                 break;
             
             case "off":
+                color = undefined;
                 parent.classList.remove("display:block");
                 parent.innerHTML = '';
                 break;
